331eb234fe52a9f4734e474bdcc41a9bfeb66c09
Kirill <timofeev.kirill.a@gmail.com>
2017-02-15 03:41:20 +0300

    add nore not null

--- create.sql
+++ create.sql
@@ -12,26 +12,26 @@ DROP TABLE IF EXISTS VendorsSell CASCADE;
 DROP TYPE IF EXISTS Gender;
 
 CREATE TABLE Components (
-  id         SERIAL PRIMARY KEY,
+  id         SERIAL       NOT NULL PRIMARY KEY,
   name       VARCHAR(100) NOT NULL UNIQUE,
   sell_price MONEY CHECK (sell_price > 0 :: MONEY) DEFAULT NULL
   --build_step SERIAL REFERENCES BuildSteps (id)
 );
 
 CREATE TABLE Machines (
-  id            SERIAL PRIMARY KEY,
+  id            SERIAL       NOT NULL PRIMARY KEY,
   name          VARCHAR(100) NOT NULL UNIQUE,
   work_stations INT          NOT NULL CHECK (work_stations >= 0)
 );
 
 CREATE TABLE BuildPlans (
-  id          SERIAL PRIMARY KEY,
+  id          SERIAL       NOT NULL PRIMARY KEY,
   name        VARCHAR(100) NOT NULL UNIQUE,
   description VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE BuildSteps (
-  id               SERIAL PRIMARY KEY,
+  id               SERIAL  NOT NULL PRIMARY KEY,
   result_component INTEGER NOT NULL REFERENCES Components (id) ON DELETE CASCADE,
   machine          INTEGER NOT NULL REFERENCES Machines (id) ON DELETE CASCADE,
   hours            INTEGER NOT NULL CHECK (hours > 0),
@@ -42,15 +42,15 @@ ALTER TABLE Components
   ADD COLUMN build_step INTEGER REFERENCES BuildSteps (id) DEFAULT NULL;
 
 CREATE TABLE BuildStepDependencies (
-  step      INTEGER REFERENCES BuildSteps (id) ON DELETE CASCADE,
-  component INTEGER REFERENCES Components (id) ON DELETE CASCADE,
+  step      INTEGER NOT NULL REFERENCES BuildSteps (id) ON DELETE CASCADE,
+  component INTEGER NOT NULL REFERENCES Components (id) ON DELETE CASCADE,
   PRIMARY KEY (step, component)
 );
 
 CREATE TYPE Gender AS ENUM ('Male', 'Female');
 
 CREATE TABLE Staff (
-  id          SERIAL PRIMARY KEY,
+  id          SERIAL      NOT NULL PRIMARY KEY,
   first_name  VARCHAR(50) NOT NULL,
   second_name VARCHAR(50) NOT NULL,
   hourly_wage MONEY       NOT NULL CHECK (hourly_wage > 0 :: MONEY AND hourly_wage <= 1000 :: MONEY),
@@ -58,31 +58,31 @@ CREATE TABLE Staff (
 );
 
 CREATE TABLE StaffCanWorkOn (
-  staff_id   INTEGER REFERENCES Staff (id) ON DELETE CASCADE,
-  machine_id INTEGER REFERENCES Machines (id) ON DELETE CASCADE,
+  staff_id   INTEGER NOT NULL REFERENCES Staff (id) ON DELETE CASCADE,
+  machine_id INTEGER NOT NULL REFERENCES Machines (id) ON DELETE CASCADE,
   PRIMARY KEY (staff_id, machine_id)
 );
 
 CREATE TABLE Vendors (
-  id   SERIAL PRIMARY KEY,
+  id   SERIAL       NOT NULL PRIMARY KEY,
   name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE VendorsSell (
-  vendor_id    INTEGER REFERENCES Vendors (id) ON DELETE CASCADE,
-  component_id INTEGER REFERENCES Components (id) ON DELETE RESTRICT,
-  price        MONEY NOT NULL,
+  vendor_id    INTEGER NOT NULL REFERENCES Vendors (id) ON DELETE CASCADE,
+  component_id INTEGER NOT NULL REFERENCES Components (id) ON DELETE RESTRICT,
+  price        MONEY   NOT NULL,
   PRIMARY KEY (vendor_id, component_id)
 );
 
 CREATE TABLE ServiceVendors (
-  id           SERIAL PRIMARY KEY,
+  id           SERIAL       NOT NULL PRIMARY KEY,
   name         VARCHAR(100) NOT NULL,
   contact_info VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE MachineServiceVendor (
-  service_vendor INTEGER REFERENCES ServiceVendors (id) ON DELETE CASCADE,
-  machine        INTEGER REFERENCES Machines (id) ON DELETE CASCADE,
+  service_vendor INTEGER NOT NULL REFERENCES ServiceVendors (id) ON DELETE CASCADE,
+  machine        INTEGER NOT NULL REFERENCES Machines (id) ON DELETE CASCADE,
   PRIMARY KEY (service_vendor, machine)
 );
