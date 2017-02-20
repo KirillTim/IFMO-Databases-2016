07df2826cc0a116b2d3643472b0e165552b333b9
Kirill <timofeev.kirill.a@gmail.com>
2017-02-20 18:45:05 +0300

    more functions

--- add_data.sql
+++ add_data.sql
@@ -3,17 +3,23 @@ INSERT INTO machines(id, name, work_stations) VALUES (1, 'machine for engine', 1
 INSERT INTO machines(id, name, work_stations) VALUES (2, 'machine for oil pipe', 1);
 INSERT INTO machines(id, name, work_stations) VALUES (3, 'machine for anything', 5);
 
-INSERT INTO staff(first_name, second_name, hourly_wage, gender) VALUES ('Ivan', 'Ivanov', 300, 'Male');
-INSERT INTO staff(first_name, second_name, hourly_wage, gender) VALUES ('Petr', 'Petrov', 400, 'Male');
-INSERT INTO staff(first_name, second_name, hourly_wage, gender) VALUES ('Sidr', 'Sidrov', 500, 'Male');
-INSERT INTO staff(first_name, second_name, hourly_wage, gender) VALUES ('Stas', 'Michailov', 350, 'Male');
-INSERT INTO staff(first_name, second_name, hourly_wage, gender) VALUES ('Anna', 'Ivanova', 600, 'Female');
+INSERT INTO staff(id, first_name, second_name, hourly_wage, gender) VALUES (1, 'Ivan', 'Ivanov', 300, 'Male');
+INSERT INTO staff(id, first_name, second_name, hourly_wage, gender) VALUES (2, 'Petr', 'Petrov', 400, 'Male');
+INSERT INTO staff(id, first_name, second_name, hourly_wage, gender) VALUES (3, 'Sidr', 'Sidrov', 500, 'Male');
+INSERT INTO staff(id, first_name, second_name, hourly_wage, gender) VALUES (4, 'Stas', 'Michailov', 350, 'Male');
+INSERT INTO staff(id, first_name, second_name, hourly_wage, gender) VALUES (5, 'Anna', 'Ivanova', 600, 'Female');
+
+INSERT INTO staffcanworkon(staff_id, machine_id) VALUES (1, 1);
+INSERT INTO staffcanworkon(staff_id, machine_id) VALUES (2, 2);
+INSERT INTO staffcanworkon(staff_id, machine_id) VALUES (3, 2);
+INSERT INTO staffcanworkon(staff_id, machine_id) VALUES (4, 3);
+INSERT INTO staffcanworkon(staff_id, machine_id) VALUES (5, 3);
 
 INSERT INTO servicevendors(id, name, contact_info) VALUES (1, 'vendor1', 'contact');
 INSERT INTO servicevendors(id, name, contact_info) VALUES (2, 'vendor2', 'contact');
 
-SELECT * FROM assign_support_contract(1, ARRAY [1,2]);
-SELECT * FROM assign_support_contract(2, ARRAY [2,3]);
+SELECT * FROM add_support_contract(1, ARRAY [1,2]);
+SELECT * FROM add_support_contract(2, ARRAY [2,3]);
 
 INSERT INTO components(name) VALUES ('spark plugs');
 INSERT INTO components(name, sell_price) VALUES ('engine', 1000);
@@ -33,3 +39,4 @@ INSERT INTO vendors(name) VALUES ('vendor #2');
 INSERT INTO vendorssell(vendor_id, component_id, price) VALUES(2, 4, 25);
 INSERT INTO vendorssell(vendor_id, component_id, price) VALUES(2, 5, 200);
 INSERT INTO vendorssell(vendor_id, component_id, price) VALUES(2, 6, 250);
+
--- functions.sql
+++ functions.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION assign_support_contract(INT, INT [])
+CREATE OR REPLACE FUNCTION add_support_contract(INT, INT [])
   RETURNS VOID AS
 $$
 DECLARE
@@ -13,5 +13,51 @@ BEGIN
     INSERT INTO machineservicevendor (service_vendor, machine) VALUES (vendor, rec.id);
   END LOOP;
   RETURN;
+END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION is_enough_staff_for_machine(INT)
+  RETURNS BOOLEAN AS
+$$
+DECLARE
+  machine ALIAS FOR $1;
+  have   INT;
+  needed INT;
+BEGIN
+  SELECT count(*)
+  INTO have
+  FROM staff
+    JOIN staffcanworkon ON staff.id = staffcanworkon.staff_id
+  WHERE staffcanworkon.machine_id = machine;
+  SELECT work_stations into needed from machines where machines.id = machine;
+  RETURN (have >= needed);
+END
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION add_build_step(INT, INT, INT, INT, INT [])
+  RETURNS VOID AS
+$$
+DECLARE
+  result ALIAS FOR $1;
+  machine_id ALIAS FOR $2;
+  hours ALIAS FOR $3;
+  plan_id ALIAS FOR $4;
+  dependencies ALIAS FOR $5;
+  cnt INT;
+  d   INT;
+BEGIN
+  --cnt := array_length(array_agg(DISTINCT dependencies), 1);
+  cnt := array_length(dependencies, 1);
+  --SELECT count(*) INTO cnt FROM components WHERE id = ANY(dependencies);
+  FOREACH d IN ARRAY dependencies LOOP
+    IF NOT exists(SELECT 1
+                  FROM components
+                  WHERE id = d)
+    THEN
+      RAISE EXCEPTION 'unknown component id: %', d;
+    END IF;
+  END LOOP;
+
+  RAISE INFO 'count= %', cnt;
 END
 $$ LANGUAGE plpgsql;
